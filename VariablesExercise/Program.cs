2f88b2ee12fa0c013a3d16f11a51d3e4a0004497
Jaylene Rodriguez <jaylenerodriguez@mac.myfiosgateway.com>
2025-05-23 14:09:41 -0400

    I suck

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace VariablesExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Depresso";
+            int age = 21;
+            char gender = 'f';
+            bool isSad = true;
+            double gPA = 3.7;
+            decimal money = 4.99m;
+
+
+            Console.WriteLine($"Name: {name}, Age: {age}, " +
+                              $"Gender: {gender}, IsSad: {isSad}, Money: {money}");
+        }
+    }
+}
